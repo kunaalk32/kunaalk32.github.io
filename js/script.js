43069d40df26c56d18e7041714407b4df6ebff34
Kunaal Kumar <Kunaal@Kunaals-MacBook-Air.local>
2018-10-13 17:38:19 -0700

    fancy effects

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 pJS_desktop = () => particlesJS.load('particles', 'js/particles.json', function() {
-  console.log('particles.js desktop loaded');
+  document.getElementsByTagName("canvas")[0].class += " fadeIn";
 });
 
 pJS_mobile = () => particlesJS.load('particles', 'js/particles_mobile.json', function() {
-  console.log('particles.js mobile loaded');
+  document.getElementsByTagName("canvas")[0].class += " fadeIn";
 });
 
+text = "Hey, I'm Kunaal";
+
+type = () => {
+  var curr = document.getElementById('typing');
+  curr_text = curr.innerText;
+  curr.innerText += text.charAt(curr_text.length);
+  if (curr.innerText.length < text.length) {
+      setTimeout(type, 50);
+  } else {
+    fadeIn();
+    if(window.innerWidth > 400){
+      pJS_desktop();
+    }
+  }
+}
+
+fadeIn = () => {
+  var elements = document.getElementsByClassName("icon");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].className += " fadeIn";
+  }
+}
+
 if(window.innerWidth > 400){
-  pJS_desktop();
+  setTimeout(type, 1500);
 }else{
   pJS_mobile();
+  document.getElementById('typing').innerText = text;
+  fadeIn();
 }
